a4dbbf4bb41a84b4f35b70ea9a08ea8578bb3495
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-04-20 12:45:40 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using LaboratorioProgra1.EjemploPolimorfismo;
+using LaboratorioProgra1.EjemploPolimorfismo.Heredados;
+using LaboratorioProgra1.Laboratorio;
+using LaboratorioProgra1.tiposdejuegos;
+
+//static void EjemploPolimorfismo()
+//{
+//    Animal[] animales = new Animal[3];
+//    animales[0] = new Tortuga();
+//    animales[1] = new Gato();
+//    animales[2] = new Perro();
+//    foreach (Animal animal in animales)
+//    {
+//        animal.HacerSonido();
+//    }
+//}   
+
+static void Laboratorio()
+{
+    VideoJuego videoJuego = new VideoJuego();
+    videoJuego.Titulo = "The Legend of Zelda: Breath of the Wild";
+    videoJuego.Desarrolladora = "Nintendo";
+    videoJuego.Calificacion = 8;
+    videoJuego.AnioLanzamiento = 2017;
+    videoJuego.MostrarInformacion();
+}
+
+Valorant valo = new Valorant();
+valo.Titulo = "Valorant";
+valo.Desarrolladora = "Riot Games";
+valo.Calificacion = 8;
+valo.AnioLanzamiento = 2020;
+valo.MostrarInformacion();
+valo.ElegirMapa();
+valo.comprararma();
+
+
+
+Console.ReadLine();
+
+//Laboratorio();+
+// VideoJuego.listaDeJuegos();
+
+
+
---
+++ tiposdejuegos/Valorant.cs
@@ -0,0 +1,36 @@
+﻿using LaboratorioProgra1.Laboratorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioProgra1.tiposdejuegos
+{
+    internal class minecraft : VideoJuego
+    {
+        public int Mundon { get; set; }
+        public int jugadorn { get; set; }
+
+        public void CrearMundo()
+        {
+            
+            Console.WriteLine("Nuevo Mundno creado");
+
+        }
+        public void ()
+        {
+            Random alear = new Random();
+            int dinero = alear.Next(700, 1200);
+            if (dinero >= 1000)
+            {
+                Console.WriteLine("Dinero suficiente para comprar un arma, ya que el arma vale 1000 y tienes"+dinero);
+
+            }
+            else { Console.WriteLine("Dinero insuficiente para comprar un arma, ya que el arma vale 1000 y tienes " +  dinero); }
+        }
+
+
+
+    }
+}
